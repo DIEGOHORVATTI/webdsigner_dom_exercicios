c97ff90619213105a9ac4640d9f7650a2150ce4c
div-leadeasy <diego-leadeasy@outlook.com.br>
2022-12-06 22:14:43 -0300

    add: normalize

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-/**
- * Use esse arquivo para resolver os exercícios
- */
+const container = document.getElementById("selectTheme");
+const buttun = document.getElementById("dark");
+
+function darkMode() {
+  let targetTheme =
+    container.getAttribute("data-theme") === "dark" ? "light" : "dark";
+
+  container.setAttribute("data-theme", targetTheme);
+
+  localStorage.setItem("theme", targetTheme);
+}
+
+function trocaMode() {
+  darkMode();
+}
